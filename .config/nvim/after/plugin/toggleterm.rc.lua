197691ce7a183f9a10fb6108fa338b830e684692
patchworkCode <dahm326@aucklanduni.ac.nz>
2022-04-21 23:28:33 +1200

    test

--- .config/nvim/after/plugin/toggleterm.rc.lua
+++ .config/nvim/after/plugin/toggleterm.rc.lua
@@ -29,14 +29,10 @@ local lazygit = Terminal:new({
     vim.cmd("startinsert!")
     vim.api.nvim_buf_set_keymap(term.bufnr, "n", "q", "<cmd>close<CR>", {noremap = true, silent = true})
   end,
-  -- function to run on closing the terminal
-  on_close = function(term)
-    vim.cmd("Closing terminal")
-  end,
 })
 
 function _lazygit_toggle()
   lazygit:toggle()
 end
 
-vim.api.nvim_set_keymap("n", "<leader>g", "<cmd>lua _lazygit_toggle()<CR>", {noremap = true, silent = true})
+vim.api.nvim_set_keymap("n", ";g", "<cmd>lua _lazygit_toggle()<CR>", {noremap = true, silent = true})
